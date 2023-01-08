a8bd37e292b58f304f7604dd79ad8f33e5a5b028
Mohamed Bahgat <mbahgat@merative.com>
2023-01-08 14:12:00 +0000

    instantiate models by name

---
+++ models/utils.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from typing import Optional
+
+from datasets.base import Dataset
+from models.base import Model, get_models_root_path
+from models.transformers import BertClassifier
+
+
+def get_model_by_name_or_none(name: str,
+							  exp_name: str,
+							  dataset: Dataset = None,
+							  models_root_path: Path = get_models_root_path()) -> Optional[Model]:
+	"""
+	Gets a model instance by name. This only works for a specific predefined list of models
+	:param name: Name of lexicon
+	:param exp_name: experiment name string
+	:param dataset: dataset used to train and eval a model. Only required if model belongs to a new experiment
+	:param models_root_path: custom models root_path
+	:return: An instance of lexicon if a corresponding type is found, else none
+	"""
+	if name == 'bert':
+		return BertClassifier(exp_name=exp_name, dataset=dataset, models_root_path=models_root_path)
+	else:
+		return None
---
+++ tests/test_models/test_utils.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import pandas as pd
+
+from datasets.base import Dataset
+from models.transformers import BertClassifier
+from models.utils import get_model_by_name_or_none
+
+
+def get_sample_dataset(datasets_root_path: Path):
+	data = {'words': ['me', 'you'], 'label': ['self', 'other']}
+	train_df = pd.DataFrame.from_dict(data)
+	valid_df = pd.DataFrame.from_dict(data)
+	test_df = pd.DataFrame.from_dict(data)
+
+	return Dataset(name='test_dataset',
+				   train_df=train_df,
+				   valid_df=valid_df,
+				   test_df=test_df,
+				   source_path='/some/path',
+				   datasets_root_path=datasets_root_path)
+
+
+def test_get_model_by_name_or_none(tmp_path):
+	dataset_root_path = tmp_path / 'datasets'
+	dataset_root_path.mkdir()
+	dataset = get_sample_dataset(dataset_root_path)
+
+	bert = get_model_by_name_or_none(name='bert', exp_name='test_exp', dataset=dataset)
+	assert type(bert) == BertClassifier
+
+	blah = get_model_by_name_or_none(name='blah', exp_name='test_exp')
+	assert blah is None
